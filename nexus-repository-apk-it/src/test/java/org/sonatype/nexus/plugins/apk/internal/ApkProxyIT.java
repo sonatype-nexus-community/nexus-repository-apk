741f0bcc596d2b227df8cbf0765097e1d4c82345
allenhsieh <allenhsiehhr@gmail.com>
2019-11-13 10:29:45 -0800

    added ITs to account for changes to PathUtils

--- nexus-repository-apk-it/src/test/java/org/sonatype/nexus/plugins/apk/internal/ApkProxyIT.java
+++ nexus-repository-apk-it/src/test/java/org/sonatype/nexus/plugins/apk/internal/ApkProxyIT.java
@@ -39,15 +39,25 @@ public class ApkProxyIT
 
   private static final String APK_INDEX = "APKINDEX.tar.gz";
 
-  private static final String APK_INDEX_URL = "v2.6/main/x86/" + APK_INDEX;
+  private static final String DIRECTORY_MAIN = "v2.6/main/x86/";
 
   private static final String APK_ARCHIVE = "a2ps-dev-4.14-r5.apk";
 
-  private static final String APK_VERSION = "4.14-r5";
+  private static final String APK_ARCHIVE_WITH_DOT = "lua5.3-5.3.5-r2.apk";
 
-  private static final String APK_COMPONENT_NAME = "a2ps-dev";
+  private static final String A2PS_VERSION = "4.14-r5";
 
-  private static final String APK_ARCHIVE_URL = "v2.6/main/x86/" + APK_ARCHIVE;
+  private static final String A2PS_COMPONENT_NAME = "a2ps-dev";
+
+  private static final String LUA_COMPONENT_NAME = "lua5.3";
+
+  private static final String LUA_VERSION = "5.3.5-r2";
+
+  private static final String PATH_APK_INDEX = DIRECTORY_MAIN + APK_INDEX;
+
+  private static final String PATH_APK_ARCHIVE = DIRECTORY_MAIN + APK_ARCHIVE;
+
+  private static final String PATH_APK_ARCHIVE_WITH_DOT = DIRECTORY_MAIN + APK_ARCHIVE_WITH_DOT;
 
   private ApkClient proxyClient;
 
@@ -66,10 +76,12 @@ public class ApkProxyIT
   @Before
   public void setup() throws Exception {
     server = Server.withPort(0)
-        .serve("/" + APK_INDEX_URL)
+        .serve("/" + PATH_APK_INDEX)
         .withBehaviours(Behaviours.file(testData.resolveFile(APK_INDEX)))
-        .serve("/" + APK_ARCHIVE_URL)
+        .serve("/" + PATH_APK_ARCHIVE)
         .withBehaviours(Behaviours.file(testData.resolveFile(APK_ARCHIVE)))
+        .serve("/" + PATH_APK_ARCHIVE_WITH_DOT)
+        .withBehaviours(Behaviours.file(testData.resolveFile(APK_ARCHIVE_WITH_DOT)))
         .start();
 
     proxyRepo = repos.createApkProxy("apk-test-proxy", server.getUrl().toExternalForm());
@@ -84,24 +96,38 @@ public class ApkProxyIT
 
   @Test
   public void retrieveApkIndexFromProxyWhenRemoteOnline() throws Exception {
-    assertThat(status(proxyClient.get(APK_INDEX_URL)), is(HttpStatus.OK));
+    assertThat(status(proxyClient.get(PATH_APK_INDEX)), is(HttpStatus.OK));
 
-    final Asset asset = findAsset(proxyRepo, APK_INDEX_URL);
-    assertThat(asset.name(), is(equalTo(APK_INDEX_URL)));
+    final Asset asset = findAsset(proxyRepo, PATH_APK_INDEX);
+    assertThat(asset.name(), is(equalTo(PATH_APK_INDEX)));
     assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
   }
 
   @Test
   public void retrieveApkArchiveFromProxyWhenRemoteOnline() throws Exception {
-    assertThat(status(proxyClient.get(APK_ARCHIVE_URL)), is(HttpStatus.OK));
+    assertThat(status(proxyClient.get(PATH_APK_ARCHIVE)), is(HttpStatus.OK));
 
-    final Asset asset = findAsset(proxyRepo, APK_ARCHIVE_URL);
-    assertThat(asset.name(), is(equalTo(APK_ARCHIVE_URL)));
+    final Asset asset = findAsset(proxyRepo, PATH_APK_ARCHIVE);
+    assertThat(asset.name(), is(equalTo(PATH_APK_ARCHIVE)));
     assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
 
-    final Component component = findComponent(proxyRepo, APK_COMPONENT_NAME);
-    assertThat(component.version(), is(equalTo(APK_VERSION)));
-    assertThat(component.name(), is(equalTo(APK_COMPONENT_NAME)));
+    final Component component = findComponent(proxyRepo, A2PS_COMPONENT_NAME);
+    assertThat(component.version(), is(equalTo(A2PS_VERSION)));
+    assertThat(component.name(), is(equalTo(A2PS_COMPONENT_NAME)));
+    assertThat(component.format(), is(equalTo(FORMAT_NAME)));
+  }
+
+  @Test
+  public void retrieveAssetWithDotInNameFromProxyWhenRemoteOnline() throws Exception {
+    assertThat(status(proxyClient.get(PATH_APK_ARCHIVE_WITH_DOT)), is(HttpStatus.OK));
+
+    final Asset asset = findAsset(proxyRepo, PATH_APK_ARCHIVE_WITH_DOT);
+    assertThat(asset.name(), is(equalTo(PATH_APK_ARCHIVE_WITH_DOT)));
+    assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
+
+    final Component component = findComponent(proxyRepo, LUA_COMPONENT_NAME);
+    assertThat(component.version(), is(equalTo(LUA_VERSION)));
+    assertThat(component.name(), is(equalTo(LUA_COMPONENT_NAME)));
     assertThat(component.format(), is(equalTo(FORMAT_NAME)));
   }
 
